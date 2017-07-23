7b766971e687013a4554c66776762fb3d42f7358
Aaron Serafin <aaron.serafin@gmail.com>
2017-07-23 17:32:38 -0400

    Added a class for entire plant information

---
+++ GreenhouseInventoryAPI/Models/CompletePlantInformation.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace GreenhouseInventoryAPI.Models
+{
+    public class CompletePlantInformation
+    {
+        public int ID;
+        public string Genus;
+        public string Species;
+        public string CommonNames;
+        public bool LowWater;
+        public bool MedWater;
+        public bool HighWater;
+        public float MaxHeight;
+        public float MaxSpread;
+        public bool CuttingPropagation;
+        public bool DivisionPropagation;
+        public bool LeafPropagation;
+        public bool Poisonous;
+        public bool Sharp;
+        public bool Irritant;
+        public string SpecialInstructions;
+    }
+}
