d5d207547a5daaf4b22dc10f5abfbf88182f13f0
Aaron Serafin <aaron.serafin@gmail.com>
2017-08-01 16:54:46 -0400

    Added outline for assigning barcode

---
+++ GreenhouseInventoryAPI/Controllers/AssignBarcodeController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using Newtonsoft.Json;
+using GreenhouseInventoryAPI.Models;
+using GreenhouseInventoryAPI.Database;
+
+namespace GreenhouseInventoryAPI.Controllers
+{
+    public class AssignBarcodeController : ApiController
+    {
+        public int Post ([FromBody] string json)
+        {
+            BarcodeAssignmentModel assignment = JsonConvert.DeserializeObject<BarcodeAssignmentModel>(json);
+            if (DBQueries.PotInfo(assignment.Barcode) == new PotInformation())
+            {
+                if (assignment.PlantID > 0)
+                {
+                    //TODO Method for inserting
+                    return 1;
+                }
+                return -1; //Improper plant ID
+            }
+                    
+            return -2; //Barcode already exists
+        }
+    }
+}
---
+++ GreenhouseInventoryAPI/Models/BarcodeAssignmentModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace GreenhouseInventoryAPI.Models
+{
+    public class BarcodeAssignmentModel
+    {
+        public int Barcode = 0;
+        public int PlantID = 0;
+        public int Location = 0;
+    }
+}
